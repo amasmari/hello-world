a11eedf9d2250367f8fa31f5ee228efef26be0f4
amasmari <65897335+amasmari@users.noreply.github.com>
2020-08-25 17:04:14 +0300

    Added the first talking script
    type: feat
    
    The first chating code
    -includes a function so the chat can be called again when needed.

---
+++ PyCodeTalk.py
@@ -0,0 +1,16 @@
+"""
+short scripted conversatoin as if the code is trying to make the user code an AI that can talk
+
+"""
+
+def LetUsTalk():
+    print("Hello there!")
+    print("I am PyTalk, a script that talks. kinda,  not real talk thoug")
+    User = str(input("Enough about me. What is your name?"))
+    print("Cool name {}! I really like that, let's become friends!".format(User))
+    print("We can have even more colorful conversations if I only knew more of your language!")
+    print(User + " hlep chat more with you and everyone! can you make me smarter?")
+
+    return None
+
+LetUsTalk()               
